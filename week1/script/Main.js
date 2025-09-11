ec2a32e0193f339af8525162a319f2ce75905f82
wweeii-code <xiazha540@gmail.com>
2025-09-11 19:57:56 +0800

    Add files via upload

---
+++ week1/script/Main.js
@@ -0,0 +1,12 @@
+﻿var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([120.6499, 24.1808]),
+    zoom: 16
+  })
+});
