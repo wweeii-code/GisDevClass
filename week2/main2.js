bb65f449031e0b739112c143f2a428e0e3e08c57
wweeii-code <xiazha540@gmail.com>
2025-09-17 11:21:12 +0800

    Add files via upload

---
+++ week2/main2.js
@@ -0,0 +1,33 @@
+﻿const defaultControls = ol.control.defaults;
+const FullScreen = ol.control.FullScreen;
+const OverviewMap = ol.control.OverviewMap;
+const ZoomSlider = ol.control.ZoomSlider;
+const DragRotateAndZoom = ol.interaction.DragRotateAndZoom;
+const defaultInteractions = ol.interaction.defaults;
+
+const source = new ol.source.OSM();
+const overviewMapControl = new OverviewMap({
+  layers: [
+    new ol.layer.Tile({
+      source: source,
+    }),
+  ],
+});
+
+const view = new ol.View({
+  center: ol.proj.fromLonLat([120.6499, 24.1808]),
+  zoom: 16
+});
+
+//new ol.Map() 建立地圖
+var map = new ol.Map({
+  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+  controls: defaultControls().extend([overviewMapControl, new FullScreen(), new ZoomSlider()]),
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: source
+    })
+  ],
+  view: view
+});
